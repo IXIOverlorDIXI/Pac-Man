2842d3b94b97094dfb522ce91d542d981e85d275
IXIOverlorDIXI <79412463+IXIOverlorDIXI@users.noreply.github.com>
2021-03-16 12:59:55 +0200

    Создание пустого проекта

---
+++ Pac-Man/Pac-Man/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pac_Man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
